13f3dc4fb15cb120ed4254009580d68fc98ac7c5
ADMINISTRADOR 1 <jf.castillog@uea.edu.ec>
2025-07-20 14:24:18 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const boton = document.querySelector('#Boton');
+    boton.addEventListener('click', function() {
+        alert('Llena todos los campos segun su requerimiento');
+    });
+});
